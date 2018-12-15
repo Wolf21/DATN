3ce8b870aeaf9f3bfaaefb6c9755f5e584ed36f0
Wolf <cgvnd123@gmail.com>
2018-12-15 09:47:49 +0700

    seeder data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(AdminUsersTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(ProDetailsTableSeeder::class);
+        $this->call(DetailImgTableSeeder::class);
+        $this->call(NewsTableSeeder::class);
+        $this->call(BannersTableSeeder::class);
+        $this->call(OdersTableSeeder::class);
+        $this->call(OdersDetailTableSeeder::class);
     }
 }
