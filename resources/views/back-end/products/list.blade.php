82224d77abd2b3ad42257dfc858a0215355d98c4
Wolf <cgvnd123@gmail.com>
2018-12-29 13:41:15 +0700

    update list all product by choose value 0 at select box

--- resources/views/back-end/products/list.blade.php
+++ resources/views/back-end/products/list.blade.php
@@ -40,6 +40,8 @@
                                             document.getElementById("inputLoai").onchange = function () {
                                                 if (this.selectedIndex !== 0) {
                                                     window.location.href = this.value;
+                                                } else {
+                                                    window.location.href = 'all';
                                                 }
                                             };
                                         </script>
