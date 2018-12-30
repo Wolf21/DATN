549d85390bc7bca067c9b0c528948b0f2aeae4ac
Wolf <cgvnd123@gmail.com>
2018-12-30 22:31:10 +0700

    update set active for leftbar

--- resources/views/back-end/modules/left-nav.blade.php
+++ resources/views/back-end/modules/left-nav.blade.php
@@ -5,40 +5,40 @@
         </div>
     </form>
     <ul class="nav menu">
-        <li class="active"><a href="{{url('admin/home/')}}">
+        <li id="home" class="active"><a href="{{url('admin/home/')}}" class="left-menu">
                 <svg class="glyph stroked dashboard-dial">
                     <use xlink:href="#stroked-dashboard-dial"></use>
                 </svg>
                 Trang chủ</a></li>
-        <li id="danhmuc"><a href="{{url('admin/category')}}">
+        <li id="category"><a href="{{url('admin/category')}}" class="left-menu">
                 <svg class="glyph stroked clipboard with paper">
                     <use xlink:href="#stroked-clipboard-with-paper"/>
                 </svg>
                 Danh mục</a></li>
 
-        <li id="sanpham"><a href="{{url('admin/product/all')}}">
+        <li id="product"><a href="{{url('admin/product/all')}}" class="left-menu">
                 <svg class="glyph stroked bag">
                     <use xlink:href="#stroked-bag"></use>
                 </svg>
                 Sản phẩm </a></li>
-        <li><a href="{{url('admin/news')}}"><span class="glyphicon glyphicon-file"></span> Tin tức</a></li>
+        <li><a href="{{url('admin/news')}}" class="left-menu"><span class="glyphicon glyphicon-file"></span> Tin tức</a></li>
 
         {{-- <li><a href="{{url('admin/nhaphang')}}"><svg class="glyph stroked download"><use xlink:href="#stroked-download"/></svg> Nhập hàng</a></li> --}}
 
-        <li><a href="{{url('admin/order')}}">
+        <li><a href="{{url('admin/order')}}" class="left-menu">
                 <svg class="glyph stroked bag">
                     <use xlink:href="#stroked-bag"></use>
                 </svg>
                 Đơn đặt hàng</a></li>
 
-        <li><a href="{{url('admin/user')}}">
+        <li><a href="{{url('admin/user')}}" class="left-menu">
                 <svg class="glyph stroked app-window">
                     <use xlink:href="#stroked-line-graph"></use>
                 </svg>
                 Khách hàng</a></li>
 
         @if(Auth()->user()->role == \App\Enums\Role::ADMIN)
-        <li><a href="{{url('admin/member')}}">
+        <li><a href="{{url('admin/member')}}" class="left-menu">
                 <svg class="glyph stroked female user">
                     <use xlink:href="#stroked-female-user"/>
                 </svg>
@@ -47,14 +47,14 @@
 
         <li role="presentation" class="divider"></li>
         {{--<li><a href="{{url('admin/khohang')}}">--}}
-        <li><a href="#">
+        <li><a href="#"  class="left-menu">
                 <svg class="glyph stroked notepad ">
                     <use xlink:href="#stroked-notepad"/>
                 </svg>
                 Thông tin kho hàng</a></li>
 
         {{--<li><a href="{{url('admin/lichsu')}}">--}}
-        <li><a href="#">
+        <li><a href="#" class="left-menu">
                 <svg class="glyph stroked table">
                     <use xlink:href="#stroked-table"/>
                 </svg>
