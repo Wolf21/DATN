614bc8af6955ccb0a1b6ba50cb664943c2361092
Wolf <cgvnd123@gmail.com>
2019-01-01 22:28:20 +0700

    fix cannot delete user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,6 +22,29 @@ class UsersController extends Controller
         return view('back-end.users.edit', ['data' => $data]);
     }
 
+    public static function getDelete($id)
+    {
+        $user = User::join('orders', 'users.id', '=', 'orders.c_id')
+            ->where('users.id', $id)
+            ->where('orders.status', 0)
+            ->get();
+        if (!count($user)) {
+            User::find($id)->delete();
+            return redirect()->back()
+                ->with([
+                    'flash_level' => 'result_msg',
+                    'flash_massage' => 'Xóa user ' . $id . ' thành công !'
+                ]);
+        } else {
+            return redirect()->back()
+                ->with([
+                    'flash_level' => 'result_msg',
+                    'flash_massage' => 'Không thể Xóa user ' . $id . ' vì còn đơn hàng chưa xác nhận !'
+                ]);
+        }
+
+
+    }
 
     public static function getUserInfo()
     {
